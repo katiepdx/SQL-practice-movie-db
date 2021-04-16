11c2f11c4d63d77bba1a1f5a730ac76265566509
katiePDX <61799774+katiepdx@users.noreply.github.com>
2021-04-16 16:24:43 -0700

    add exercise 3

--- exercise_3/exercise-3.test.js
+++ exercise_3/exercise-3.test.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const pool = require('../lib/utils/pool');
 
-describe.skip('exercise 3', () => {
+describe('exercise 3', () => {
   afterAll(() => pool.end());
 
   it('find all film titles with the category Action', async () => {
